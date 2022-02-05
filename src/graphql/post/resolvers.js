9ac49d6fd2bbea19d8a6a368a366a323a761a2d2
PauloDev13 <devpgm2020@gmail.com>
2022-02-05 16:53:18 -0300

    Using UnionTypes

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -4,15 +4,36 @@ const posts = async (_, { input }, { getPosts }) => {
   const response = await getPosts(`?${apiFilterInput}`);
   return response.json();
 };
+
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts(id);
-  return response.json();
+  const post = await response.json();
+  console.log(post.id);
+
+  if (post.id === undefined) {
+    return {
+      statusCode: 404,
+      message: `Post não encontrado para o ID: ${id}.`,
+    };
+  }
+
+  return post;
 };
 
 export const postResolvers = {
   Query: {
     post,
     posts,
+  },
+
+  PostResult: {
+    __resolveType: (obj) => {
+      console.log('OBJECT ' + typeof obj.statusCode);
+      if (typeof obj.statusCode !== 'undefined') return 'PostNotFoundError';
+      if (typeof obj.id !== 'undefined') return 'Post';
+
+      return null;
+    },
   },
 
   Post: {
