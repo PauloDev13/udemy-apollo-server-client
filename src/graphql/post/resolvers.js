c2278ae37aaaa198ef4c7d1eaa8d83b654889e52
PauloDev13 <devpgm2020@gmail.com>
2022-02-05 17:29:02 -0300

    Refactoring the code for section 4 of the course

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -7,55 +7,12 @@ const posts = async (_, { input }, { getPosts }) => {
 
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts(id);
-  const post = await response.json();
-
-  if (Math.random() > 0.5) {
-    return {
-      statusCode: 500,
-      message: `Servidor demorou muito para responder.`,
-      timeout: 123,
-    };
-  }
-
-  if (post.id === undefined) {
-    return {
-      statusCode: 404,
-      message: `Post não encontrado para o ID: ${id}.`,
-      postId: id,
-    };
-  }
-
-  return post;
+  return response.json();
 };
 
 export const postResolvers = {
   Query: {
     post,
     posts,
-  },
-
-  PostResult: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-      if (typeof obj.id !== 'undefined') return 'Post';
-
-      return null;
-    },
-  },
-
-  PostError: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-
-      return null;
-    },
-  },
-
-  Post: {
-    winTimestamps: () => {
-      return 'Fernanda';
-    },
   },
 };
