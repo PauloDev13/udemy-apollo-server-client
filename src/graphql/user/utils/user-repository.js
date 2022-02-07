db81b8b70fc8934024c831372705f6086d31205f
PauloDev13 <devpgm2020@gmail.com>
2022-02-07 14:48:20 -0300

    Adding passwordHash field in User

--- src/graphql/user/utils/user-repository.js
+++ src/graphql/user/utils/user-repository.js
@@ -1,15 +1,18 @@
-import { ValidationError } from 'apollo-server';
-
+import { ValidationError, UserInputError } from 'apollo-server';
+import bcrypt from 'bcrypt';
 // CRIA User
 export const createUserFn = async (userData, dataSource) => {
-  var userInfo = await createUserInfo(userData, dataSource);
-  const { firstName, lastName, userName } = userInfo;
+  const { firstName, lastName, userName, password } = userData;
 
-  if (!firstName || !lastName || !userName) {
+  var userInfo = await createUserInfo(userData, dataSource);
+
+  if (!firstName || !lastName || !userName || !password) {
     throw new ValidationError(
-      'Informe Nome, Sobrenome e o Login do usuário para concluir o cadastro',
+      'Informe Nome, Sobrenome, Login e Senha do usuário para concluir o cadastro',
     );
   }
+
+  validateUserPassword(password);
 
   var userFound = await isUserDuplicate(userName, dataSource);
   if (typeof userFound !== 'undefined') {
@@ -26,7 +29,7 @@ export const updateUserFn = async (userId, userData, dataSource) => {
 
   await userExist(userId, dataSource);
 
-  const { firstName, lastName, userName } = userData;
+  const { firstName, lastName, userName, password } = userData;
 
   if (typeof firstName !== 'undefined') {
     if (!firstName) {
@@ -46,10 +49,20 @@ export const updateUserFn = async (userId, userData, dataSource) => {
     }
   }
 
+  if (typeof password !== 'undefined') {
+    if (!password) {
+      throw new ValidationError('A Senha deve ser informado');
+    } else {
+      validateUserPassword(password);
+    }
+  }
+
   var userFound = await isUserDuplicate(userName, dataSource);
   if (typeof userFound !== 'undefined' && userFound.id !== userId) {
     throw new ValidationError(`Já existe um usuário com o Login ${userName}`);
   }
+
+  await encryptPassword(userData);
 
   return await dataSource.patch(`/users/${userId}`, { ...userData });
 };
@@ -65,6 +78,8 @@ export const deleteUserFn = async (userId, dataSource) => {
 };
 
 // MÉTODOS AUXILIARES
+
+// verifica se o usuário existe no BD
 const userExist = async (userId, dataSource) => {
   try {
     await dataSource.context.dataSources.usersAPI.get(`/users/${userId}`);
@@ -73,6 +88,7 @@ const userExist = async (userId, dataSource) => {
   }
 };
 
+// verifica se já existe no BD usuário com o mesmo Login
 const isUserDuplicate = async (userName, dataSource) => {
   const found = await dataSource.get('/users', {
     userName,
@@ -80,8 +96,20 @@ const isUserDuplicate = async (userName, dataSource) => {
   return found[0];
 };
 
+// valida a senha para que tenha letras maiúscula, minúsculas, números e entre
+// 6 e 16 caracteres
+const validateUserPassword = (password) => {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,15}$/;
+  if (!password.match(passwordRegex)) {
+    throw new UserInputError(
+      'A Senha deve letras maiúsculas, minúsculas, números e 6 a 15 caracteres',
+    );
+  }
+};
+
+// atualiza os campos indexRef e createdAt antes de salvar e atualizar um usuário
 const createUserInfo = async (userData, dataSource) => {
-  // const { firstName, lastName, userName } = userData;
+  await encryptPassword(userData);
 
   const indexRefUser = await dataSource.get('/users', {
     _limit: 1,
@@ -96,4 +124,15 @@ const createUserInfo = async (userData, dataSource) => {
     indexRef,
     createdAt: new Date().toISOString(),
   };
+};
+
+// criptografa a senha, caso ela ainda não tenha sido criptografada e adiciona
+// o campo passwordHash e exclui o cam password dos dados enviados para o BD
+const encryptPassword = async (userData) => {
+  if (userData.password && !userData.passwordHash) {
+    const { password } = userData;
+    const passwordHash = await bcrypt.hash(password, 12);
+    userData.passwordHash = passwordHash;
+    delete userData['password'];
+  }
 };
