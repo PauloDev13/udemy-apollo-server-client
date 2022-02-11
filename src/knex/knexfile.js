77cda7e82586ce6bd68eeabee08f67d62dd65f91
PauloDev13 <devpgm2020@gmail.com>
2022-02-11 16:17:51 -0300

    Creating a Migration in Knex Query Builder

--- src/knex/knexfile.js
+++ src/knex/knexfile.js
@@ -2,7 +2,7 @@ const { resolve } = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config({
-  path: resolve(__dirname, '...', '...', 'env'),
+  path: resolve(__dirname, '..', '..', '.env'),
 });
 
 module.exports = {
